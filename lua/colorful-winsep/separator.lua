01bdccb887a24c5906a70f77f1b008f5a7cece0c
Grzegorz Ulatowski <grzegorz.ulatowski@plusserver.com>
2026-02-18 22:34:39 +0100

    refactor(separator): optimize initialization logic and clean up code

--- lua/colorful-winsep/separator.lua
+++ lua/colorful-winsep/separator.lua
@@ -57,8 +57,9 @@ function Separator:vertical_init(height, highlight_start, highlight_end)
   self.window.width = 1
 
   local content = { self.start_symbol }
+  local body = self.body_symbol
   for i = 2, height - 1 do
-    content[i] = self.body_symbol
+    content[i] = body
   end
   content[height] = self.end_symbol
   api.nvim_buf_set_lines(self.buffer, 0, -1, false, content)
@@ -71,7 +72,6 @@ function Separator:vertical_init(height, highlight_start, highlight_end)
     api.nvim_buf_set_extmark(self.buffer, ns_id, 0, 0, {
       end_row = 1,
       end_col = #self.start_symbol - 1,
-      -- end_col = 1,
       hl_group = "ColorfulWinSepStart",
     })
   end
@@ -80,7 +80,6 @@ function Separator:vertical_init(height, highlight_start, highlight_end)
     api.nvim_buf_set_extmark(self.buffer, ns_id, height - 1, 0, {
       end_row = height - 1,
       end_col = #self.end_symbol - 1,
-      -- end_col = 1,
       hl_group = "ColorfulWinSepEnd",
     })
   end
@@ -94,41 +93,37 @@ end
 function Separator:horizontal_init(width, highlight_start, highlight_end, title)
   self.window.height = 1
   self.window.width = width
-  local start_symbol = self.start_symbol
-  local end_symbol = self.end_symbol
-  local body_symbol = self.body_symbol
 
   local ns_id = api.nvim_create_namespace("colorful-winsep-symbols")
   api.nvim_buf_clear_namespace(self.buffer, ns_id, 0, -1)
 
-  local line_content = ""
-  local title_start_byte = -1
-  local title_end_byte = -1
+  local start_s, body_s, end_s = self.start_symbol, self.body_symbol, self.end_symbol
+  local start_w, end_w = vim.fn.strwidth(start_s), vim.fn.strwidth(end_s)
+  local body_w = width - start_w - end_w
 
-  if title and title ~= "" and config.opts.header and config.opts.header.enabled then
+  local line_content
+  local title_start_byte, title_end_byte = -1, -1
+
+  local header_cfg = config.opts.header
+  if title and title ~= "" and header_cfg and header_cfg.enabled then
     local display_title = " " .. title .. " "
     local title_w = vim.fn.strwidth(display_title)
-    local start_w = vim.fn.strwidth(start_symbol)
-    local end_w = vim.fn.strwidth(end_symbol)
-    local body_w = width - start_w - end_w
 
     if title_w <= body_w then
       local left_pad_w = math.floor((body_w - title_w) / 2)
       local right_pad_w = body_w - title_w - left_pad_w
 
-      local left_body = string.rep(body_symbol, left_pad_w)
-      local right_body = string.rep(body_symbol, right_pad_w)
+      local left_body = string.rep(body_s, left_pad_w)
+      local right_body = string.rep(body_s, right_pad_w)
 
-      line_content = start_symbol .. left_body .. display_title .. right_body .. end_symbol
-      title_start_byte = #start_symbol + #left_body
+      line_content = start_s .. left_body .. display_title .. right_body .. end_s
+      title_start_byte = #start_s + #left_body
       title_end_byte = title_start_byte + #display_title
-    else
-      line_content = start_symbol .. string.rep(body_symbol, body_w) .. end_symbol
     end
-  else
-    local start_w = vim.fn.strwidth(start_symbol)
-    local end_w = vim.fn.strwidth(end_symbol)
-    line_content = start_symbol .. string.rep(body_symbol, width - start_w - end_w) .. end_symbol
+  end
+
+  if not line_content then
+    line_content = start_s .. string.rep(body_s, body_w) .. end_s
   end
 
   api.nvim_buf_set_lines(self.buffer, 0, -1, false, { line_content })
@@ -136,7 +131,7 @@ function Separator:horizontal_init(width, highlight_start, highlight_end, title)
   -- Highlight start
   if highlight_start ~= false then
     api.nvim_buf_set_extmark(self.buffer, ns_id, 0, 0, {
-      end_col = #start_symbol,
+      end_col = #start_s,
       hl_group = "ColorfulWinSepStart",
     })
   end
@@ -145,13 +140,13 @@ function Separator:horizontal_init(width, highlight_start, highlight_end, title)
   if title_start_byte ~= -1 then
     api.nvim_buf_set_extmark(self.buffer, ns_id, 0, title_start_byte, {
       end_col = title_end_byte,
-      hl_group = config.opts.header.highlight or "ColorfulWinSepHeader",
+      hl_group = header_cfg.highlight or "ColorfulWinSepHeader",
     })
   end
 
   -- Highlight end
   if highlight_end ~= false then
-    api.nvim_buf_set_extmark(self.buffer, ns_id, 0, #line_content - #end_symbol, {
+    api.nvim_buf_set_extmark(self.buffer, ns_id, 0, #line_content - #end_s, {
       end_col = #line_content,
       hl_group = "ColorfulWinSepEnd",
     })
